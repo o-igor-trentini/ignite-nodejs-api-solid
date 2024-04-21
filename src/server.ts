133f9d866e51c7476cd9b9d312b775b94d398535
Igor Trentini <igortrentini.2004@gmail.com>
2024-04-21 19:22:19 -0300

    feat: Variáveis de ambiente e validação

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => console.log('Servidor rodando em: http://localhost:3333'))
