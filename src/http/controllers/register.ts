54e1b38fdb7b4d5d231e31f501e5d4d37755d989
Igor Trentini <igortrentini.2004@gmail.com>
2024-04-26 19:51:35 -0300

    feat: Separação de rotas e controllers

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export const register = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+}
